a32050e36e2685c5d4663593bcc69265606aea44
Ahmad Ubaidillah Tsani <ahmadubai02@gmail.com>
2026-09-24 10:24:08 +0700

    feat(projects): add image assets for project 8 and 9

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -155,6 +155,8 @@ import project4 from "../assets/project4.png";
 import project5 from "../assets/project5.png";
 import project6 from "../assets/project6.png";
 import project7 from "../assets/project7.png";
+import project8 from "../assets/project8.png";
+import project9 from "../assets/project9.png";
 
 const modules = [Autoplay, Pagination, Navigation];
 
